0c9ca157312141b7ba65de4b842f2e5774c9f036
ZafirChowdhury <zafirchowdhury69@gmail.com>
2026-06-05 16:22:29 +0600

    Add countdown func

---
+++ coutdown.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func coutdown(duration time.Duration, msg string) {
+	for duration >= 0 {
+		// \r go to the start of the line
+		// \033[K erase everythong on the lin
+
+		fmt.Print("\r\033[K")
+		fmt.Print("\r")
+		fmt.Printf("%s%d", msg, int(duration.Seconds()))
+
+		time.Sleep(time.Second)
+		duration -= time.Second
+	}
+}
--- main.go
+++ main.go
@@ -7,17 +7,6 @@ import (
 
 func main() {
 	duration := 10 * time.Second
-
-	for duration >= 0 {
-		// \r go to the start of the line
-		// \033[K erase everythong on the line
-		fmt.Print("\r\033[K")
-
-		fmt.Printf("\rTime remaning: %d seconds", int(duration.Seconds()))
-
-		time.Sleep(time.Second)
-		duration -= time.Second
-	}
-
+	coutdown(duration, "Time remaning: ")
 	fmt.Println("\nDone")
 }
